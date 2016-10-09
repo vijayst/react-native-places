7b272aa6dc2fffff2382161d85c9bbbaac1b179a
vijayst <vijayst@gmail.com>
2016-10-09 09:57:44 +0530

    Tab selection.

--- Places/index.ios.js
+++ Places/index.ios.js
@@ -7,18 +7,33 @@ import {
 } from 'react-native';
 
 class Places extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selectedTab: 0
+    };
+  }
+
+  handleTabPress(tab) {
+    this.setState({ selectedTab: tab })
+  }
+
   render() {
     return (
     <TabBarIOS>
         <TabBarIOS.Item
           systemIcon="favorites"
-          selected={true}
+          selected={this.state.selectedTab === 0}
+          onPress={this.handleTabPress.bind(this, 0)}
         >
           <Text style={styles.text}>Favorite Places</Text>
         </TabBarIOS.Item>
         <TabBarIOS.Item
           title="Place"
           icon={require('./assets/pin.png')}
+          selected={this.state.selectedTab === 1}
+          onPress={this.handleTabPress.bind(this, 1)}
         >
           <Text style={styles.text}>Add Place</Text>
         </TabBarIOS.Item>
